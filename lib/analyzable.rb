def9c96a6e59044511e2e052a982cf7c81f8d16c
Thomas Davids <tfdavids@gmail.com>
2016-04-29 17:10:07 -0700

    analyzable passes tests

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,26 @@
 module Analyzable
-  # Your code goes here!
+  def average_price(objs)
+    (objs.map{|obj| obj.price.to_f}.inject(:+) / objs.length).round(2)
+  end
+
+  def print_report(objs)
+    report = "Inventory by Brand:\n"
+    brands = count_by_brand(objs)
+    brands.each{|k,v| report += "  - #{k}: #{v}\n"}
+    names = count_by_name(objs)
+    names.each{|k,v| report += "  - #{k}: #{v}\n"}
+    report
+  end
+
+  def count_by_brand(objs)
+    brands = {}
+    objs.each {|obj| brands[obj.brand] = (brands[obj.brand] || 0) + 1}
+    brands
+  end
+
+  def count_by_name(objs)
+    names = {}
+    objs.each {|obj| names[obj.name] = (names[obj.name] || 0) + 1}
+    names
+  end
 end
